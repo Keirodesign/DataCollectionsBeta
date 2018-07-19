b87a69855e472da77bafe132548214b080ab92fc
Andy Barton <ajb31@outlook.com>
2018-07-19 13:12:08 +0100

    Iteration changes, first commit

--- app/config.js
+++ app/config.js
@@ -7,7 +7,7 @@ module.exports = {
   serviceName: 'Submit learner data',
 
   // Default port that prototype runs on
-  port: '3000',
+  port: '6000',
 
   // Enable or disable password protection on production
   useAuth: 'true',
