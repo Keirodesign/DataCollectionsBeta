21aa76c89e2fbae481f648f3a3aa8c33035ecb5a
Andy Barton <ajb31@outlook.com>
2018-07-30 11:40:47 +0100

    Updated contents, and renamed two ESF files for consistency

--- app/routes.js
+++ app/routes.js
@@ -10,13 +10,19 @@ router.get('/', function (req, res) {
 router.get('/9-1-0/choose_file_to_upload', function (req, res) {
   // Get the answer from the query string (eg. ?whattosubmit=ilr)
   var whattosubmit = req.query.whattosubmit
-
   if (whattosubmit === 'esf') {
-    // Redirect to the relevant page
-    res.redirect('/9-1-0/choose_file_to_upload_ESFA')
+  	res.redirect('esf_choose_file_to_upload')
   } else {
-    // If any other value (or is missing) render the page requested
-   	res.render('9-1-0/choose_file_to_upload')
+  	res.render('9-1-0/choose_file_to_upload')
+  } 
+})
+
+router.get('/9-1-0/your_files_uploading', function (req, res) {
+  var whattosubmit = req.query.whattosubmit
+  if (whattosubmit === 'esf') {
+    res.render('9-1-0/your_files_uploading', { 'uploadingFile': 'ESF-file-submission-Marj-2018.csv', 'continueTo': './esf_data_submitted.html' })
+  } else {
+  	res.render('9-1-0/your_files_uploading', { 'uploadingFile': 'ILR-file-submission-March-2018.xml', 'continueTo': './theres_a_problem.html' })
   }
 })
 
