4a9245d9c65d65861580d058ca599ed7f55354d1
Andy Barton <ajb31@outlook.com>
2018-08-07 08:31:00 +0100

    EAS Screenshots

--- app/routes.js
+++ app/routes.js
@@ -71,5 +71,26 @@ router.get('/9-2-5/your_files_uploading', function (req, res) {
   }
 })
 
+// --------------------------  Iteration 9-3-0  -------------------------------------------------------------- 
+
+router.get('/9-3-0/choose_file_to_upload', function (req, res) {
+  // Get the answer from the query string (eg. ?whattosubmit=ilr)
+  var whattosubmit = req.query.whattosubmit
+  if (whattosubmit === 'esf') {
+    res.redirect('esf_choose_file_to_upload')
+  } else {
+    res.render('9-3-0/choose_file_to_upload')
+  } 
+})
+
+router.get('/9-3-0/your_files_uploading', function (req, res) {
+  var whattosubmit = req.query.whattosubmit
+  if (whattosubmit === 'esf') {
+    res.render('9-3-0/your_files_uploading', { 'uploadingFile': 'SUPPDATA-12345678-ESF-8976-20180806-151209.CSV', 'continueTo': './esf_data_submitted.html' })
+  } else {
+    res.render('9-3-0/your_files_uploading', { 'uploadingFile': 'ILR-12345678-1819-20180806-151209-02.XML', 'continueTo': './theres_a_problem.html' })
+  }
+})
+
 
 module.exports = router
