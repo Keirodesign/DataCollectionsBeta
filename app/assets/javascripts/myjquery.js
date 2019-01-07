ce3b29dd4fc37e6217dba310b4a9c158cdeaeaf9
Arif Liya <arifliya@gmail.com>
2019-01-07 14:38:19 +0000

    errors update 7th Jane

--- app/assets/javascripts/myjquery.js
+++ app/assets/javascripts/myjquery.js
@@ -1,4 +1,17 @@
 $('.govuk-button').click(function() {
    if($('#fix-errors').is(':checked')) { location.href='/12-0-0/choose_file_to_upload'; }
 	 else if ($('#submit-file').is(':checked')) { location.href='/12-0-0/ilr_data_submitted'; }
+
+   else if(!$('.govuk-radios__item input').is(':checked')) {
+     $('#formError').addClass('govuk-form-group--error');
+     $('.govuk-error-summary').removeClass('hide');
+     $('.govuk-error-message').removeClass('hide');
+   }
+
+});
+
+$('.govuk-radios__item input').change(function(){
+  $('#formError').removeClass('govuk-form-group--error');
+  $('.govuk-error-summary').addClass('hide');
+  $('.govuk-error-message').addClass('hide');
 });
