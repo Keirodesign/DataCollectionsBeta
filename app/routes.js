634a9017e93a61a82dc285c7713c4c225dc88118
Arif Liya <arifliya@gmail.com>
2019-01-15 13:10:38 +0000

    links tweaked on 11-03

--- app/routes.js
+++ app/routes.js
@@ -506,6 +506,27 @@ router.get('/11-2-0/choose_file_to_upload', function (req, res) {
     }
 })
 
+// --------------------------  Iteration 11-3-0  --------------------------------------------------------------
+
+router.get('/11-3-0/choose_file_to_upload', function (req, res) {
+  // Get the answer from the query string (eg. ?whattosubmit=ilr)
+  var whattosubmit = req.query.whattosubmit
+  if (whattosubmit === 'reports') {
+        res.redirect('return-period')
+      }
+    else {
+  if (whattosubmit === 'esf') {
+    res.redirect('esf_choose_file_to_upload')
+  } else {
+      if (whattosubmit === 'eas') {
+        res.redirect('eas_choose_file_to_upload')
+      } else {
+        res.render('11-2-0/choose_file_to_upload')
+      }
+  }
+    }
+})
+
 // --------------------------  Iteration 12-0-0  --------------------------------------------------------------
 
 router.get('/12-0-0/choose_file_to_upload', function (req, res) {
@@ -526,6 +547,8 @@ router.get('/12-0-0/choose_file_to_upload', function (req, res) {
   }
     }
 });
+
+
 
 
 
