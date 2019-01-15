6b254ae843ebcf5ea950fae4ab51c5a565647402
Arif Liya <arifliya@gmail.com>
2019-01-15 10:29:32 +0000

    roll back to previous version with updates and renamed to 11-3-0

--- app/assets/javascripts/myjquery.js
+++ app/assets/javascripts/myjquery.js
@@ -1,3 +1,4 @@
+// to add a URL to the button when any radio button is select //
 $('.govuk-button').click(function() {
    if($('#fix-errors').is(':checked')) { location.href='/12-0-0/choose_file_to_upload'; }
 	 else if ($('#submit-file').is(':checked')) { location.href='/12-0-0/ilr_data_submitted'; }
@@ -7,7 +8,6 @@ $('.govuk-button').click(function() {
      $('.govuk-error-summary').removeClass('hide');
      $('.govuk-error-message').removeClass('hide');
    }
-
 });
 
 $('.govuk-radios__item input').change(function(){
